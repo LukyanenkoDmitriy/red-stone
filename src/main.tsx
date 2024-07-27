dc971c440a266b89043d331ad2dd7e1461dde18a
LukyanenkoDmitriy <lukyanenkodmitriyal@gmail.com>
2024-07-27 20:42:52 +0300

    Check commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './global.scss'
+import {Home} from "@/pages/home";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<>Hey</>
-	</React.StrictMode>
+    <React.StrictMode>
+        <Home/>
+    </React.StrictMode>
 )
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,11 @@
+import {Button} from "@/components/ui/button/Button.tsx";
+
+export const Home = () => {
+    return (
+        <>
+            <h1>Start game</h1>
+            <Button variant='secondary'>start</Button>
+        </>
+    );
+};
+
